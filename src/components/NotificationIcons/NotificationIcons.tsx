bf9fcd15fce34b69e41106d37244fecbeca3fd72
Sushil Ingole <sushil.ingole@nihilent.com>
2024-08-05 14:26:05 +0530

    Updated profile icon

--- src/components/NotificationIcons/NotificationIcons.tsx
+++ src/components/NotificationIcons/NotificationIcons.tsx
@@ -2,6 +2,7 @@ import { CiMail } from "react-icons/ci";
 import "./NotificationIcons.scss";
 import { IoSettingsOutline } from "react-icons/io5";
 import { VscBellDot } from "react-icons/vsc";
+import { CgProfile } from "react-icons/cg";
 
 const NotificationIcon = () => {
   return (
@@ -9,7 +10,7 @@ const NotificationIcon = () => {
         <span className="noti-icons"><CiMail/></span>
         <span className="noti-icons"><IoSettingsOutline/></span>
         <span className="noti-icons"><VscBellDot/></span>
-        <span className="noti-icons"><CiMail/></span>
+        <span className="noti-icons"><CgProfile/></span>
     </div>
   )
 }
